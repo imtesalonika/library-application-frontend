bdc9ece3048aa00a8c80771709a9507bd93b2d5d
Tesalonika Aprisda Sitopu <tesalonilaaprisdasitopu@gmail.com>
2025-02-17 16:11:22 +0700

    menghubungkan autentikasi dengan API CIS

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import 'dotenv/config';
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import 'dotenv/config'
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default ({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
 
-  const { VITE_NODE_ENV } = process.env;
+  const { VITE_NODE_ENV } = process.env
 
   return defineConfig({
     mode: VITE_NODE_ENV,
@@ -21,5 +21,5 @@ export default ({ mode }) => {
         '@pages': path.resolve(__dirname, './src/pages'),
       },
     },
-  });
-};
+  })
+}
