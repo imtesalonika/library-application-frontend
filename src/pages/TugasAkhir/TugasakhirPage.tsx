fbdb4bccf7a7d46cc67bdf7c554c9e380c26d9f9
Tesalonika Aprisda Sitopu <tesalonilaaprisdasitopu@gmail.com>
2025-03-17 20:58:40 +0700

    menambah docker file

--- src/pages/TugasAkhir/TugasakhirPage.tsx
+++ src/pages/TugasAkhir/TugasakhirPage.tsx
@@ -1,161 +1,172 @@
-import DataTable from 'datatables.net-react';
-import DT, { Config } from 'datatables.net-bs4';
-import 'datatables.net-responsive-bs4';
-import 'datatables.net-buttons-bs4';
-import { useEffect, useState } from "react";
-import { Plus, PencilSquare, Trash, BoxArrowUpRight } from "react-bootstrap-icons";
-import { Link, useNavigate } from "react-router-dom";
-import withReactContent from 'sweetalert2-react-content';
-import Swal from 'sweetalert2';
-import { apiUrl } from '@app/utils/env';
-import axios from 'axios';
+import DataTable from 'datatables.net-react'
+import DT, { Config } from 'datatables.net-bs4'
+import 'datatables.net-responsive-bs4'
+import 'datatables.net-buttons-bs4'
+import { useEffect, useState } from 'react'
+import {
+  Plus,
+  PencilSquare,
+  Trash,
+  BoxArrowUpRight,
+} from 'react-bootstrap-icons'
+import { Link, useNavigate } from 'react-router-dom'
+import withReactContent from 'sweetalert2-react-content'
+import Swal from 'sweetalert2'
+import { apiUrl } from '@app/utils/env'
+import axios from 'axios'
 
-DataTable.use(DT);
+DataTable.use(DT)
 
 export function TugasakhirPage() {
-    const [tugasakhirData, setTugasAkhirData] = useState<any>()
-    const MySwal = withReactContent(Swal)
-    const navigation = useNavigate()
+  const [tugasakhirData, setTugasAkhirData] = useState<any>()
+  const MySwal = withReactContent(Swal)
+  const navigation = useNavigate()
 
-    const getTugasAkhirData = async () => {
-        try {
-          const response = await axios.get(`${apiUrl}/api/tugasakhir`)
-          console.error(response.data.data);
-          setTugasAkhirData(response.data.data)
-        } catch (e: any) {
-          console.log(e)
-        }
+  const getTugasAkhirData = async () => {
+    try {
+      const response = await axios.get(`${apiUrl}/api/tugasakhir/get-all`)
+      console.error(response.data.data)
+      setTugasAkhirData(response.data.data)
+    } catch (e: any) {
+      console.log(e)
     }
+  }
 
-    useEffect(() => {
+  useEffect(() => {
+    getTugasAkhirData().then()
+  }, [])
+
+  const handleRemoveTugasAkhir = async (tugasakhirId: number) => {
+    try {
+      const response = await axios.delete(
+        `${apiUrl}/api/tugasakhir/${tugasakhirId}`
+      )
+
+      if (response.status === 200) {
+        MySwal.fire({
+          text: response.data.message,
+          icon: 'success',
+          title: 'Success!',
+        }).then()
         getTugasAkhirData().then()
-    }, [])
-
-    const handleRemoveTugasAkhir = async (tugasakhirId: number) => {
-        try {
-          const response = await axios.delete(`${apiUrl}/api/tugasakhir/${tugasakhirId}`)
-    
-          if (response.status === 200) {
-            MySwal.fire({
-              text: response.data.message,
-              icon: 'success',
-              title: 'Success!',
-            }).then()
-            getTugasAkhirData().then()
-          }
-        } catch (e: any) {
-          MySwal.fire({
-            text: e.response.data.message,
-            icon: 'error',
-            title: 'Failed!',
-          }).then()
-          console.log(e)
-        }
+      }
+    } catch (e: any) {
+      MySwal.fire({
+        text: e.response.data.message,
+        icon: 'error',
+        title: 'Failed!',
+      }).then()
+      console.log(e)
     }
+  }
 
-    const tableOption: Config = {
-        ordering: true,
-        paging: true,
-        searching: true,
-        info: true,
-        responsive: true,
-    }
+  const tableOption: Config = {
+    ordering: true,
+    paging: true,
+    searching: true,
+    info: true,
+    responsive: true,
+  }
 
-    return (
-        <div className={'p-4 bg-white'}>
-            <h2>Tugas Akhir</h2>
-            <div className={'d-flex mb-3 w-100'} style={{ gap: 10 }}>
-                <Link to={"/tugasakhir/add"}>
-                    <button className="btn btn-success d-flex align-items-center" style={{
-                        gap: 3
-                    }}><Plus/> Add Tugas Akhir
-                    </button>
-                </Link>
-            </div>
+  return (
+    <div className={'p-4 bg-white'}>
+      <h2>Tugas Akhir</h2>
+      <div className={'d-flex mb-3 w-100'} style={{ gap: 10 }}>
+        <Link to={'/tugasakhir/add'}>
+          <button
+            className="btn btn-success d-flex align-items-center"
+            style={{
+              gap: 3,
+            }}
+          >
+            <Plus /> Add Tugas Akhir
+          </button>
+        </Link>
+      </div>
 
-            {tugasakhirData !== undefined ? (
-            <DataTable
-            options={tableOption}
-            className={'table table-bordered table-hover mt-3'}
-            >
-                <thead>
-                    <tr>
-                        <th>No.</th>
-                        <th>Judul Tesis</th>
-                        <th>Prodi</th>
-                        <th>Tindakan</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {tugasakhirData?.map((row: any, index: any) => (
-                        <tr key={index}>
-                            <td>{index + 1}</td>
-                            <td>{row.judul}</td>
-                            <td>{row.prodi}</td>
-                            <td className={'d-flex'} style={{ gap: '5px' }}>
-                                <button
-                                    className="btn btn-success d-flex align-items-center"
-                                    onClick={() => {
-                                    navigation(`/tugasakhir/${row.id}`)
-                                    }}
-                                >
-                                    <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="16"
-                                    height="16"
-                                    fill="currentColor"
-                                    className="bi bi-arrow-up-right"
-                                    viewBox="0 0 16 16"
-                                    >
-                                    <path
-                                        fill-rule="evenodd"
-                                        d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z"
-                                    />
-                                    </svg>
-                                </button>
-                                <button
-                                    className="btn btn-warning d-flex align-items-center"
-                                    onClick={() => {
-                                    navigation(`/tugasakhir/edit/${row.id}`)
-                                    }}
-                                >
-                                    <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="16"
-                                    height="16"
-                                    fill="currentColor"
-                                    className="bi bi-pencil"
-                                    viewBox="0 0 16 16"
-                                    >
-                                    <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325" />
-                                    </svg>
-                                </button>
-                                <button
-                                    className="btn btn-danger d-flex align-items-center"
-                                    onClick={() => {
-                                    handleRemoveTugasAkhir(row.id).then()
-                                    }}
-                                >
-                                    <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="16"
-                                    height="16"
-                                    fill="currentColor"
-                                    className="bi bi-trash"
-                                    viewBox="0 0 16 16"
-                                    >
-                                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
-                                    <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
-                                    </svg>
-                                </button>
-                                </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </DataTable>
-            ) : (
-                ''
-            )}
-        </div>
-    );
+      {tugasakhirData !== undefined ? (
+        <DataTable
+          options={tableOption}
+          className={'table table-bordered table-hover mt-3'}
+        >
+          <thead>
+            <tr>
+              <th>No.</th>
+              <th>Judul Tesis</th>
+              <th>Prodi</th>
+              <th>Tindakan</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tugasakhirData?.map((row: any, index: any) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{row.judul}</td>
+                <td>{row.prodi}</td>
+                <td className={'d-flex'} style={{ gap: '5px' }}>
+                  <button
+                    className="btn btn-success d-flex align-items-center"
+                    onClick={() => {
+                      navigation(`/tugasakhir/${row.id}`)
+                    }}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      className="bi bi-arrow-up-right"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z"
+                      />
+                    </svg>
+                  </button>
+                  <button
+                    className="btn btn-warning d-flex align-items-center"
+                    onClick={() => {
+                      navigation(`/tugasakhir/edit/${row.id}`)
+                    }}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      className="bi bi-pencil"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325" />
+                    </svg>
+                  </button>
+                  <button
+                    className="btn btn-danger d-flex align-items-center"
+                    onClick={() => {
+                      handleRemoveTugasAkhir(row.id).then()
+                    }}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      className="bi bi-trash"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
+                      <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
+                    </svg>
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </DataTable>
+      ) : (
+        ''
+      )}
+    </div>
+  )
 }
