b9d0cb41ed7124ba3d41918e60c481869b8c78d9
Gerry Bukit <ifs21055@students.del.ac.id>
2025-03-24 16:21:24 +0700

    search pinjambuku

--- src/pages/Buku/PinjamBuku.tsx
+++ src/pages/Buku/PinjamBuku.tsx
@@ -2,9 +2,16 @@ import { toast } from 'react-toastify'
 import axios from 'axios'
 import { apiUrl } from '@app/utils/env'
 import { useEffect, useState } from 'react'
-// import { formatWaktu } from '@app/services/format-waktu';
 import { CheckLg, XLg } from 'react-bootstrap-icons'
 import moment from 'moment-timezone'
+import DataTable from 'datatables.net-react'
+import DT, { Config } from 'datatables.net-bs4'
+import 'datatables.net-responsive-bs4'
+import 'datatables.net-buttons-bs4'
+
+// Initialize DataTable with DT
+DataTable.use(DT)
+
 
 export default function PinjamBuku() {
   const [dataPeminjaman, setDataPeminjaman] = useState([])
@@ -68,123 +75,138 @@ export default function PinjamBuku() {
     getDataPeminjaman()
   }, [])
 
+  const tableOption: Config = {
+    ordering: true,
+    paging: true,
+    searching: true,
+    info: true,
+    responsive: true,
+  }
+
   return (
     <div className={'p-4 bg-white'}>
       <h1>Peminjaman Buku</h1>
-
-      <table className={'table table-bordered table-hover mt-3'}>
-        <thead>
+  
+      {dataPeminjaman?.length > 0 ? (
+        <DataTable
+          options={tableOption}
+          className={'table table-bordered table-hover mt-3'}
+        >
+          <thead>
           <tr>
-            <th>No.</th>
-            <th>Judul Buku</th>
-            <th>Peminjam</th>
-            <th>Status</th>
-            <th>Tanggal Pinjam</th>
-            <th>Batas Peminjaman</th>
-            <th>Tanggal Kembali</th>
-            <th>Tindakan</th>
-          </tr>
-        </thead>
-        <tbody>
-          {dataPeminjaman?.length > 0 ? (
-            dataPeminjaman.map((row: any, index: number) => (
-              <tr key={index}>
-                <td>{index + 1}</td>
-                <td>{row.judul_buku}</td>
-                <td>{row.nama_peminjam}</td>
-                <td>{row.status_peminjaman}</td>
-                <td>{formatWaktu(row.tanggal_pinjam)}</td>
-                <td>{formatWaktu(row.batas_peminjaman)}</td>
-                <td>
-                  {row.tanggal_kembali ? formatWaktu(row.tanggal_kembali) : '-'}
-                </td>
-                <td>
-                  {row.status_peminjaman === 'REQ' ? (
-                    <div className={'d-flex'} style={{ gap: '5px' }}>
-                      <button
-                        className="btn btn-success btn-sm"
-                        onClick={() =>
-                          handleAcceptRejectPeminjaman(
-                            row.id_peminjaman,
-                            'IS BEING BORROWED',
-                            row.id_buku
-                          )
-                        }
-                      >
-                        <CheckLg />
-                      </button>
-                      <button
-                        onClick={() =>
-                          handleAcceptRejectPeminjaman(
-                            row.id_peminjaman,
-                            'REJECTED',
-                            row.id_buku
-                          )
-                        }
-                        className="btn btn-danger btn-sm"
-                      >
-                        <XLg />
-                      </button>
-                    </div>
-                  ) : row.status_peminjaman === 'DONE' ||
-                    row.status_peminjaman === 'REJECTED' ? (
-                    '-'
-                  ) : (
-                    <div
-                      className={'d-flex flex-wrap'}
-                      style={{
-                        gap: 5,
-                      }}
-                    >
-                      
-                      <button
-                        onClick={() =>
-                          handleAcceptRejectPeminjaman(
-                            row.id_peminjaman,
-                            'REJECTED',
-                            row.id_buku
-                          )
-                        }
-                        className="btn btn-danger btn-sm"
-                      >
-                        Tidak diambil
-                      </button>
-                      <button
-                        className="btn btn-success btn-sm"
-                        onClick={() => {
-                          handlePerpanjang(row.id_peminjaman).then()
+      <th style={{ width: '3%' }}>No.</th>
+      <th style={{ width: '25%' }}>Judul Buku</th>
+      <th style={{ width: '13%' }}>Peminjam</th>
+      <th style={{ width: '12%' }}>Status</th>
+      <th style={{ width: '10%' }}>Tanggal Pinjam</th>
+      <th style={{ width: '10%' }}>Batas Peminjaman</th>
+      <th style={{ width: '10%' }}>Tanggal Kembali</th>
+      <th style={{ width: '27%' }}>Tindakan</th>
+    </tr>
+          </thead>
+          <tbody>
+            {dataPeminjaman?.length > 0 ? (
+              dataPeminjaman.map((row: any, index: number) => (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>{row.judul_buku}</td>
+                  <td>{row.nama_peminjam}</td>
+                  <td>{row.status_peminjaman}</td>
+                  <td>{formatWaktu(row.tanggal_pinjam)}</td>
+                  <td>{formatWaktu(row.batas_peminjaman)}</td>
+                  <td>
+                    {row.tanggal_kembali ? formatWaktu(row.tanggal_kembali) : '-'}
+                  </td>
+                  <td>
+                    {row.status_peminjaman === 'REQ' ? (
+                      <div className={'d-flex'} style={{ gap: '5px' }}>
+                        <button
+                          className="btn btn-success btn-sm"
+                          onClick={() =>
+                            handleAcceptRejectPeminjaman(
+                              row.id_peminjaman,
+                              'IS BEING BORROWED',
+                              row.id_buku
+                            )
+                          }
+                        >
+                          <CheckLg />
+                        </button>
+                        <button
+                          onClick={() =>
+                            handleAcceptRejectPeminjaman(
+                              row.id_peminjaman,
+                              'REJECTED',
+                              row.id_buku
+                            )
+                          }
+                          className="btn btn-danger btn-sm"
+                        >
+                          <XLg />
+                        </button>
+                      </div>
+                    ) : row.status_peminjaman === 'DONE' ||
+                      row.status_peminjaman === 'REJECTED' ? (
+                      '-'
+                    ) : (
+                      <div
+                        className={'d-flex flex-wrap'}
+                        style={{
+                          gap: 5,
                         }}
                       >
-                        Perpanjang
-                      </button>
-                      <button
-                        className="btn btn-primary btn-sm"
-                        onClick={() =>
-                          handleAcceptRejectPeminjaman(
-                            row.id_peminjaman,
-                            'DONE',
-                            row.id_buku
-                          )
-                        }
-                      >
-                        Selesai
-                      </button>
-                    </div>
-                  )}
+                        <button
+                          onClick={() =>
+                            handleAcceptRejectPeminjaman(
+                              row.id_peminjaman,
+                              'REJECTED',
+                              row.id_buku
+                            )
+                          }
+                          className="btn btn-danger btn-sm"
+                        >
+                          Tidak diambil
+                        </button>
+                        <button
+                          className="btn btn-success btn-sm"
+                          onClick={() => {
+                            handlePerpanjang(row.id_peminjaman).then();
+                          }}
+                        >
+                          Perpanjang
+                        </button>
+                        <button
+                          className="btn btn-primary btn-sm"
+                          onClick={() =>
+                            handleAcceptRejectPeminjaman(
+                              row.id_peminjaman,
+                              'DONE',
+                              row.id_buku
+                            )
+                          }
+                        >
+                          Selesai
+                        </button>
+                      </div>
+                    )}
+                  </td>
+                </tr>
+              ))
+            ) : (
+              <tr>
+                <td colSpan={8} className="text-center">
+                  Tidak ada data!
                 </td>
               </tr>
-            ))
-          ) : (
-            <tr>
-              <td colSpan={7} className="text-center">
-                Tidak ada data!
-              </td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+            )}
+          </tbody>
+        </DataTable>
+      ) : (
+        <p className="text-center mt-3">Tidak ada data peminjaman!</p>
+      )}
     </div>
-  )
+  );
+  
 }
 
 export function formatWaktu(waktu: any) {
